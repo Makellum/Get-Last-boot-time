1e74689b972440565286f48eceddaeaa7170c0b0
Amber Kellum <makellum@gmail.com>
2021-04-28 12:10:36 -0400

    Add files via upload

---
+++ Get-LastBootTime.ps1
@@ -0,0 +1,16 @@
+﻿<#
+Get-LastBootTime -ComputerName localhost
+Find your computer name: Settings>System>About
+#>
+
+
+
+param(
+[Parameter(Mandatory=$true)][string]$ComputerName
+)
+
+Get-WmiObject -Class Win32_OperatingSystem –ComputerName localhost |
+
+Select-Object -Property CSName,@{n=”Last Booted”;
+
+e={[Management.ManagementDateTimeConverter]::ToDateTime($_.LastBootUpTime)}}
